0863bdb97607e71d57578e5ae5a437118ebe86ab
ecnryptionStudios <calvin.heiniger@outlook.com>
2025-08-03 08:41:08 -0700

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,21 @@
  * 
  * To register a constrol simmessages, use addSimMessageHandler
  */
+addSimMessageHandler("rfid", (data) => {
+    switch (data.action) {
+        case "write":
+            console.log("Writing to tag:", data.data)
+            window.localStorage.setItem("rfid_data", data.data)
+            break
+
+        case "read":
+            const stored = window.localStorage.getItem("rfid_data") || ""
+            const msg = JSON.stringify({
+                action: "readResult",
+                data: stored
+            })
+            const buf = pxsim.BufferMethods.createBufferFromUTF8(msg)
+            pxsim.control.simmessages.send("rfid", buf)
+            break
+    }
+})
